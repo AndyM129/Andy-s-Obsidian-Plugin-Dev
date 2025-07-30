b848ca717cf23a6cad27d9ed3b15396574b926d2
mengxinxin <mengxinxin@baidu.com>
2025-07-30 21:57:16 +0800

    调研

--- .obsidian/plugins/obsidian-sample-plugin/main.ts
+++ .obsidian/plugins/obsidian-sample-plugin/main.ts
@@ -1,203 +1,38 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	WorkspaceLeaf,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+import { Plugin } from "obsidian";
 
-import { ExampleView, VIEW_TYPE_EXAMPLE } from "./view/ExampleView";
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
-};
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class DailyWorksPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		console.log("DailyWorks 插件已加载");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
+		this.registerMarkdownCodeBlockProcessor(
+			"daily-works",
+			(source, el, ctx) => {
+				const dateLine = source
+					.split("\n")
+					.find((line) => line.trim().startsWith("- date:"));
+				const dateMatch = dateLine?.match(
+					/- date:\s*(\d{4}-\d{2}-\d{2})/
+				);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
+				if (dateMatch) {
+					const date = dateMatch[1];
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					// 创建自定义渲染元素
+					const dateDiv = el.createEl("div", {
+						text: `🗓️ 日期：${date}`,
+					});
+					dateDiv.style.padding = "8px";
+					dateDiv.style.backgroundColor = "#f4f4f4";
+					dateDiv.style.borderRadius = "6px";
+					dateDiv.style.fontWeight = "bold";
+				} else {
+					el.createEl("div", { text: "⚠️ 未找到 date 参数" });
 				}
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
-
-		// This creates an icon in the left ribbon.
-		const exampleViewIconEl = this.addRibbonIcon(
-			"dice",
-			"Example View",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				this.activateView();
 			}
 		);
-		// Perform additional things with the ribbon
-		exampleViewIconEl.addClass("my-plugin-ribbon-class");
-
-		this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
 	}
 
-	onunload() {}
-
-	async activateView() {
-		const { workspace } = this.app;
-
-		let leaf: WorkspaceLeaf | null = null;
-		const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
-
-		if (leaves.length > 0) {
-			// A leaf with our view already exists, use that
-			leaf = leaves[0];
-			workspace.revealLeaf(leaf);
-		} else {
-			// Our view could not be found in the workspace, create a new leaf in the right sidebar for it
-			leaf = workspace.getLeaf(true);
-			leaf.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true }).then(
-				() => leaf && workspace.revealLeaf(leaf)
-			);
-		}
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-
-		containerEl.createEl("h1", { text: "Heading 1" });
-
-		// const book = containerEl.createEl("div");
-		// book.createEl("div", { text: "How to Take Smart Notes" });
-		// book.createEl("small", { text: "Sönke Ahrens" });
-
-		const book = containerEl.createEl("div", { cls: "book" });
-		book.createEl("div", {
-			text: "How to Take Smart Notes",
-			cls: "book__title",
-		});
-		book.createEl("small", { text: "Sönke Ahrens", cls: "book__author" });
+	onunload() {
+		console.log("DailyWorks 插件已卸载");
 	}
 }
